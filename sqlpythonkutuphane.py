f5f39cac1fdee232a3a848b3c3ae98983ff10edf
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2023-12-25 21:52:56 +0300

    Update sqlpythonkutuphane.py

--- sqlpythonkutuphane.py
+++ sqlpythonkutuphane.py
@@ -1,87 +1,139 @@
 import sqlite3
+from tkinter import Tk, Label, Button, Text, Scrollbar, END, Entry, StringVar
 
-def main():
-    conn = sqlite3.connect("kutuphane.db")
+class KutuphaneUygulamasi:
+    def __init__(self, ana_pencere):
+        self.ana_pencere = ana_pencere
+        self.ana_pencere.title("Kütüphane Uygulaması")
 
-    # menü
-    while True:
-        print("** Kitaplık Uygulaması **")
-        print("1. Kitapları listele")
-        print("2. Yeni kitap ekle")
-        print("3. Kitaplarda arama yap")
-        print("4. Kitap sil")
-        print("5. Kitap güncelle")
-        print("6. Çıkış")
-        secim = input("Seçiminiz: ")
-        if secim == "1":
-            listele()
-        elif secim == "2":
-            ekle()
-        elif secim == "3":
-            arama()
-        elif secim == "4":
-            sil()
-        elif secim == "5":
-            guncelle()
-        elif secim == "6":
-            break
+        # Etiketler
+        self.etiket = Label(ana_pencere, text="Yapmak İstediğiniz İşlemi Seçin:")
+        self.etiket.grid(row=0, column=0, columnspan=2, pady=10)
 
-def listele():
-    # tablodan veri çek
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM kutuphane")
-    kutuphane = cur.fetchall()
+        # Butonlar
+        self.ara_button = Button(ana_pencere, text="Eser Ara", command=self.eser_ara_pencere)
+        self.ara_button.grid(row=1, column=0, pady=5, padx=10)
 
-    # veri listeleme
-    for eser in kutuphane:
-        print(eser)
+        self.listele_button = Button(ana_pencere, text="Eser Listele", command=self.eser_listele)
+        self.listele_button.grid(row=1, column=1, pady=5, padx=10)
 
-def ekle():
-    # Eser bilgilerini kullanıcıdan al.
-    eser_adi = input("Eser adı: ")
-    eser_yazari = input("Eser yazarı: ")
-    eser_yayinevi = input("Eser yayınevi: ")
-    eser_konu_basligi = input("Eser konu başlığı: ")
+        self.ekle_button = Button(ana_pencere, text="Yeni Eser Ekle", command=self.yeni_eser_ekle_pencere)
+        self.ekle_button.grid(row=2, column=0, pady=5, padx=10)
 
-    # veri ekleme
-    cur = conn.cursor()
-    cur.execute("INSERT INTO kutuphane (eser_adi, eser_yazari, eser_yayinevi, eser_konu_basligi) VALUES (?, ?, ?, ?)", (eser_adi, eser_yazari, eser_yayinevi, eser_konu_basligi))
-    conn.commit()
-    print("Eser başarıyla eklendi.")
+        self.sil_button = Button(ana_pencere, text="Eser Sil", command=self.eser_sil_pencere)
+        self.sil_button.grid(row=2, column=1, pady=5, padx=10)
 
-def arama():
-    anahtar_kelime = input("Arama anahtar kelimesi: ")
+        # Metin Kutusu ve Kaydırma Çubuğu
+        self.metin_kutusu = Text(ana_pencere, height=10, width=50)
+        self.metin_kutusu.grid(row=3, column=0, columnspan=2, pady=10, padx=10)
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM kutuphane WHERE eser_adi LIKE ? OR eser_yazari LIKE ? OR eser_konu_basligi LIKE ?", (f"%{anahtar_kelime}%", f"%{anahtar_kelime}%", f"%{anahtar_kelime}%"))
-    kutuphane = cur.fetchall()
+        self.kaydirma_cubugu = Scrollbar(ana_pencere, command=self.metin_kutusu.yview)
+        self.kaydirma_cubugu.grid(row=3, column=2, sticky="nsew")
 
-    for eser in kutuphane:
-        print(eser)
+        self.metin_kutusu.config(yscrollcommand=self.kaydirma_cubugu.set)
 
-def sil():
-    silme_anahtar_kelime = input("Silmek istediğiniz eserin adını girin: ")
+    def eser_ara_pencere(self):
+        pencere = Tk()
+        pencere.title("Eser Ara")
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM kutuphane WHERE eser_adi = ?", (silme_anahtar_kelime,))
-    kutuphane = cur.fetchall()
+        etiket = Label(pencere, text="Aranacak Bilgiyi Girin:")
+        etiket.grid(row=0, column=0, padx=10, pady=5)
 
-    # eser var mı kontrol et
-    if len(kutuphane) == 0:
-        print("Böyle bir kitap yok.")
-        return
+        arama_entry = Entry(pencere)
+        arama_entry.grid(row=0, column=1, padx=10, pady=5)
 
-    # eseri sil.
-    cur.execute("DELETE FROM kutuphane WHERE eser_adi = ?", (silme_anahtar_kelime,))
-    conn.commit()
-    print("Eser başarıyla silindi.")
+        ara_button = Button(pencere, text="Ara", command=lambda: self.eser_ara(arama_entry.get()))
+        ara_button.grid(row=1, column=0, columnspan=2, pady=10)
 
-def guncelle():
-    eser_adi = input("Güncellemek istediğiniz eserin adını girin: ")
+    def eser_ara(self, arama_kelimesi):
+        self.ekran_yazisi("Aranan Eser:")
+        self.metin_kutusu.delete(1.0, END)
+        conn = sqlite3.connect("kutuphane.db")
+        cursor = conn.cursor()
+        arama_kelimesi_kucuk = arama_kelimesi.lower()
+        # Tüm alanlarda arama yapmak için LIKE ve % işaretini kullanıyoruz
+        cursor.execute("SELECT * FROM kutuphane WHERE LOWER(eser) LIKE ? OR LOWER(yazar) LIKE ? OR LOWER(yayinevi) LIKE ? OR LOWER(konubasligi) LIKE ?",
+                       (f"%{arama_kelimesi_kucuk}%", f"%{arama_kelimesi_kucuk}%", f"%{arama_kelimesi_kucuk}%", f"%{arama_kelimesi_kucuk}%"))
+        eserler = cursor.fetchall()
+        conn.close()
+        if eserler:
+            for eser in eserler:
+                self.metin_kutusu.insert(END, f"Eser Adı: {eser[0]}\nYazar: {eser[1]}\nYayınevi: {eser[2]}\nKonu Başlığı: {eser[3]}\n\n")
+        else:
+            self.metin_kutusu.insert(END, "Eser bulunamadı.\n")
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM kutuphane WHERE eser_adi = ?", (eser_adi,))
-    kutuphane = cur.fetchall()
+    def yeni_eser_ekle_pencere(self):
+        pencere = Tk()
+        pencere.title("Yeni Eser Ekle")
 
-    if len(kutuphane) == 0:
-        print
+        etiketler = ["Yeni Eser Adı:", "Yazar Adı:", "Yayınevi:", "Konu Başlığı:"]
+        entry_değerleri = []
+
+        for i, etiket in enumerate(etiketler):
+            Label(pencere, text=etiket).grid(row=i, column=0, padx=10, pady=5)
+            entry = Entry(pencere)
+            entry.grid(row=i, column=1, padx=10, pady=5)
+            entry_değerleri.append(entry)
+
+        ekle_button = Button(pencere, text="Ekle", command=lambda: self.yeni_eser_ekle(entry_değerleri))
+        ekle_button.grid(row=len(etiketler), column=0, columnspan=2, pady=10)
+
+    def yeni_eser_ekle(self, entry_değerleri):
+        degerler = [entry.get() for entry in entry_değerleri]
+        if all(degerler):
+            conn = sqlite3.connect("kutuphane.db")
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO kutuphane VALUES (?, ?, ?, ?)", tuple(degerler))
+            conn.commit()
+            conn.close()
+            self.ekran_yazisi(f"{degerler[0]} adlı eser başarıyla eklendi.")
+        else:
+            self.ekran_yazisi("Lütfen tüm alanları doldurun.")
+
+    def eser_sil_pencere(self):
+        pencere = Tk()
+        pencere.title("Eser Sil")
+
+        etiket = Label(pencere, text="Silinecek Eser Adını Girin:")
+        etiket.grid(row=0, column=0, padx=10, pady=5)
+
+        eser_adı_entry = Entry(pencere)
+        eser_adı_entry.grid(row=0, column=1, padx=10, pady=5)
+
+        sil_button = Button(pencere, text="Sil", command=lambda: self.eser_sil(eser_adı_entry.get()))
+        sil_button.grid(row=1, column=0, columnspan=2, pady=10)
+
+    def eser_sil(self, eser_adı):
+        if eser_adı:
+            conn = sqlite3.connect("kutuphane.db")
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM kutuphane WHERE eser=?", (eser_adı,))
+            conn.commit()
+            conn.close()
+            self.ekran_yazisi(f"{eser_adı} adlı eser başarıyla silindi.")
+        else:
+            self.ekran_yazisi("Lütfen bir eser adı girin.")
+
+    def eser_listele(self):
+        self.ekran_yazisi("Kütüphane Verileri:")
+        veriler = self.tum_verileri_getir()
+        self.metin_kutusu.delete(1.0, END)
+        for eser in veriler:
+            self.metin_kutusu.insert(END, f"Eser Adı: {eser[0]}\nYazar: {eser[1]}\nYayınevi: {eser[2]}\nKonu Başlığı: {eser[3]}\n\n")
+
+    def tum_verileri_getir(self):
+        conn = sqlite3.connect("kutuphane.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM kutuphane")
+        veriler = cursor.fetchall()
+        conn.close()
+        return veriler
+
+    def ekran_yazisi(self, mesaj):
+        self.metin_kutusu.delete(1.0, END)
+        self.metin_kutusu.insert(END, mesaj)
+
+if __name__ == "__main__":
+    root = Tk()
+    uygulama = KutuphaneUygulamasi(root)
+    root.mainloop()
