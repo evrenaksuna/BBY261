b1e2e235a5e5ce952ae1425bcbb742e60c6cf24e
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2023-12-26 11:53:59 +0300

    Update kutuphane.py

--- kutuphane.py
+++ kutuphane.py
@@ -1,5 +1,5 @@
 import sqlite3
-from tkinter import Tk, Label, Button, Text, Scrollbar, END, Entry, StringVar
+from tkinter import Tk, Label, Button, Text, Scrollbar, END, Entry
 
 class KutuphaneUygulamasi:
     def __init__(self, ana_pencere):
